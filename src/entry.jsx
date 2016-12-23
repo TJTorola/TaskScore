232ae358054fd4e53d997222cdb332489ad36455
Tyler Torola <tyler@tjt.codes>
2016-12-23 12:51:04 -0800

    reconfigured component architecture

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Root from 'components/root';
+import Root from 'components/base/root';
 
 document.addEventListener("DOMContentLoaded", () => {
 	const main = document.querySelector('main');
