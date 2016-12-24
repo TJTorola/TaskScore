f0cbaaab4fbd870b6af3149e4a7320e5571523d7
Tyler Torola <tyler@tjt.codes>
2016-12-23 22:45:12 -0800

    adds score and nextId tests

--- test/reducers/tasks/nextId.js
+++ test/reducers/tasks/nextId.js
@@ -1,5 +1,23 @@
 import test from 'ava';
 
 import reducer from 'reducers/tasks/nextId';
+import { 
+	addTask,
+	removeTask
+} from 'actions/tasks';
 
-test.todo("'ADD_TASK' incraments the nextId");
+const initial = reducer(undefined, {}),
+      added   = reducer(initial, addTask(0, 'task', 81923812784)),
+      removed = reducer(added, removeTask(0));
+
+test("Initializes to 0.", t => {
+	t.is(initial, 0);
+});
+
+test("'ADD_TASK' incraments the nextId.", t => {
+	t.is(added, 1);
+});
+
+test("'REMOVE_TASK' has no effect.", t => {
+	t.is(removed, 1);
+});
--- test/reducers/tasks/score.js
+++ test/reducers/tasks/score.js
@@ -1,6 +1,23 @@
 import test from 'ava';
 
 import reducer from 'reducers/tasks/score';
+import { 
+	incramentScore,
+	decrementScore
+} from 'actions/tasks';
 
-test.todo("'INCREMENT_SCORE' increases the score by one.");
-test.todo("'DECREMENT_SCORE' decreases the score by one.");
+const initial     = reducer(undefined, {}),
+      incramented = reducer(initial, incramentScore()),
+      decramented = reducer(incramented, decrementScore());
+
+test("Inializes to 0.", t => {
+	t.is(initial, 0);
+});
+
+test("'INCREMENT_SCORE' increases the score by one.", t => {
+	t.is(incramented, 1);
+});
+
+test("'DECREMENT_SCORE' decreases the score by one.", t => {
+	t.is(decramented, 0);
+});
