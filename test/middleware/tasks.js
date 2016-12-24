7556b527d465e5b7eb62726e6b4122edc95eb1aa
Tyler Torola <tyler@tjt.codes>
2016-12-23 23:23:48 -0800

    starts tasks middleware tester

--- test/middleware/tasks.js
+++ test/middleware/tasks.js
@@ -1,7 +1,40 @@
 import test from 'ava';
 
-test.todo("'BUILD_TASK' takes a description then emits 'ADD_TASK'.");
-test.todo("'BUILD_TASK' adds time to task.");
-test.todo("'BUILD_TASK' adds nextId to task.");
-test.todo("'FINISH_TASK' emits 'ADD_FINISHED_TASK' if task found in current.");
-test.todo("'FINISH_TASK' does nothing if no task found.");
+import tasks from 'middleware/tasks';
+import {
+	buildTask,
+	finishTask
+} from 'actions/tasks';
+
+const mockStore = ({
+	getState: () => ({
+		tasks: {
+			nextId: 0,
+			score: 0,
+			current: [],
+			finished: []
+		}
+	}),
+	dispatch: action => () => action
+});
+const middleware = tasks(mockStore)(() => {});
+
+test("'BUILD_TASK' takes a description then dispatch 'ADD_TASK'.", t => {
+	
+});
+
+test("'BUILD_TASK' adds time to task.", t => {
+
+});
+
+test("'BUILD_TASK' adds nextId to task.", t => {
+
+});
+
+test("'FINISH_TASK' dispatch 'ADD_FINISHED_TASK' if task found in current.", t => {
+
+});
+
+test("'FINISH_TASK' does nothing if no task found.", t => {
+
+});
