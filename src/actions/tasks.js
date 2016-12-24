7be0419a5cb05d0987de6497319af5d11736b960
Tyler Torola <tyler@tjt.codes>
2016-12-23 21:34:03 -0800

    adds tasks actions

---
+++ src/actions/tasks.js
@@ -0,0 +1,33 @@
+export const buildTask = str => ({
+	type: "BUILD_TASK",
+	description: str
+});
+
+export const addTask = (str, int) => ({
+	type: "ADD_TASK",
+	description: str,
+	created: int
+});
+
+export const finishTask = int => ({
+	type: "FINISH_TASK",
+	id: int
+});
+
+export const removeTask = int => ({
+	type: "REMOVE_TASK",
+	id: int
+});
+
+export const incramentScore = () => ({
+	type: "INCREMENT_SCORE"
+});
+
+export const decrementScore = () => ({
+	type: "DECREMENT_SCORE"
+});
+
+export const expireFinishedTasks = int => ({
+	type: "EXPIRE_FINISHED_TASKS",
+	time: int
+});
