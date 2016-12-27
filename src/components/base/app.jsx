236aef743dd81f00ab53adcb7bfae8131d23cadd
Tyler Torola <tyler@tjt.codes>
2016-12-26 22:59:23 -0800

    properly formats app component

--- src/components/base/app.jsx
+++ src/components/base/app.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import Tasks from './tasks/view';
 import Finished from './finished/view';
 
-export default () => <div>
-	<Tasks />
-	<Finished />
-</div>;
+export default () => (
+	<div>
+		<Tasks />
+		<Finished />
+	</div>
+);
