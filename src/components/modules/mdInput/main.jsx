09e61db077713489cba378273f39b043564b7466
Tyler Torola <tyler@tjt.codes>
2016-12-26 23:19:13 -0800

    fixes placeholder, finishes input

--- src/components/modules/mdInput/main.jsx
+++ src/components/modules/mdInput/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default props => (
-	<div className="md-input"> 
-		<input { ...props } required />
-		<span className="highlight"></span>
-		<span className="bar"></span>
-		<label>{ props.placeholder }</label>
-	</div>
-);
+export default props => {
+	const { placeholder, ...rest } = props;
+
+	return (
+		<div className="md-input"> 
+			<input { ...rest } required />
+			<label>{ placeholder }</label>
+		</div>
+	);
+};
