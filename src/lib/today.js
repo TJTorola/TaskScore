f74731d044dcc031019836f4d2f7215a6c48c5c0
Tyler Torola <tyler@tjt.codes>
2016-12-27 11:36:09 -0800

    adds daily experation to init hook

--- src/lib/today.js
+++ src/lib/today.js
@@ -1,13 +1,21 @@
 const CUTOFF_HOUR = 4;
 
+const hrsToMs = hrs => 1000 * 60 * 60 * hrs;
+const ONE_DAY = hrsToMs(24);
+
 const today = () => {
 	const stamp = Date.now();
-	return Math.floor(stamp / (1000 * 60 * 60 * 24));
+	return Math.floor(stamp / ONE_DAY);
 };
 
 export const todaysCutoff = () => {
-	const dayNum = today(),
-	      dayStamp = dayNum * 1000 * 60 * 60 * 24;
+	const dayNum     = today(),
+	      dayStamp   = dayNum * ONE_DAY,
+	      cutoffTime = dayStamp + hrsToMs(CUTOFF_HOUR);
 
-	return dayStamp + (CUTOFF_HOUR * 1000 * 60 * 60);
+	if (cutoffTime < Date.now()) {
+		return cutoffTime;
+	} else {
+		return cutoffTime - ONE_DAY;
+	}
 };
--- src/store/initialize.js
+++ src/store/initialize.js
@@ -1,5 +1,6 @@
 import { todaysCutoff } from 'lib/today';
+import { expireTasks } from 'actions/tasks';
 
 export default store => {
-	
+	store.dispatch(expireTasks(todaysCutoff()));
 }
