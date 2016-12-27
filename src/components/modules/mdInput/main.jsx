182dbe7124347057c03fc635afa238ec66b56d4d
Tyler Torola <tyler@tjt.codes>
2016-12-26 23:14:14 -0800

    starts adding mdInput scss

--- src/components/modules/mdInput/main.jsx
+++ src/components/modules/mdInput/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default props => (
 	<div className="md-input"> 
-		<input { ...props } />
+		<input { ...props } required />
 		<span className="highlight"></span>
 		<span className="bar"></span>
 		<label>{ props.placeholder }</label>
